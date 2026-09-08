73ac2d9cb0ee5a8b2e3b37743692bac36a79b2f1
sakuratwhr <sakura.tw.hr@gmail.com>
2026-09-08 08:46:53 +0800

    Rename V2 frontend theme to online monthly meeting

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 // 系統設定集中管理
 const APP_CONFIG = {
   GAS_API_URL: "https://script.google.com/macros/s/AKfycby3TccTngcW6B0n5MbRqrztJlg9nciD86m1rurTSbHSNhvFAW2H1DwQqwBvdnK8VhAu/exec",
-  EVENT_NAME: "年度內部大會",
+  EVENT_NAME: "線上月會",
   CHECK_INTERVAL_MS: 30000
 };
