9be40e30a05abbdeaeffa5d38a5fcdf3888c0e37
sakuratwhr <sakura.tw.hr@gmail.com>
2026-09-10 14:20:26 +0800

    設定數位學習推薦課程連結

--- js/config.js
+++ js/config.js
@@ -4,3 +4,14 @@ const APP_CONFIG = {
   EVENT_NAME: "線上月會",
   CHECK_INTERVAL_MS: 30000
 };
+
+// 數位學習推薦課程連結
+// 未來只需要修改這裡的網址，不需要修改 index.html
+const LEARNING_LINKS = {
+  1: "https://sakura.elearn.com.tw/cltcms/app-get-session-url.do?courseType=1&sessionId=588&courseId=311&isProgram=0&psessionId=0",
+  2: "",
+  3: "https://sakura.elearn.com.tw/cltcms/app-get-session-url.do?courseType=1&sessionId=620&courseId=334&isProgram=0&psessionId=0",
+  4: "https://sakura.elearn.com.tw/cltcms/app-get-session-url.do?courseType=1&sessionId=587&courseId=318&isProgram=0&psessionId=0",
+  5: "https://sakura.elearn.com.tw/cltcms/app-get-session-url.do?courseType=1&sessionId=579&courseId=316&isProgram=0&psessionId=0",
+  6: "https://sakura.elearn.com.tw/cltcms/app-get-session-url.do?courseType=1&sessionId=561&courseId=310&isProgram=0&psessionId=0"
+};
